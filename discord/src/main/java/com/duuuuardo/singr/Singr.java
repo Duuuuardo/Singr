8014d46ed03f64ecf8f1b3a2a9d1ce18ec0e50fe
Duuuuardo <oeduardoef@gmail.com>
2023-07-16 19:33:38 -0300

    Running the bot and setting up some logs!

--- discord/src/main/java/com/duuuuardo/singr/Singr.java
+++ discord/src/main/java/com/duuuuardo/singr/Singr.java
@@ -1,7 +1,29 @@
 package com.duuuuardo.singr;
 
+import com.duuuuardo.singr.utils.config.DiscordConfig;
+import net.dv8tion.jda.api.sharding.DefaultShardManagerBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Singr {
+  private DiscordConfig config;
+
+  public Singr(DiscordConfig config) {
+    super();
+    this.config = config;
+  }
+
+  private static Logger logger = LoggerFactory.getLogger(Singr.class);
+
   public void start() {
-    System.out.println("Hello from Singr");
+    DefaultShardManagerBuilder builder =
+        DefaultShardManagerBuilder.create(config.discordToken, config.intents)
+            .setShardsTotal(config.maxShards)
+            .setStatus(config.status)
+            .setBulkDeleteSplittingEnabled(false)
+            .setRawEventsEnabled(true);
+
+    logger.info("Connecting SingR to discord!");
+    builder.build();
   }
 }
--- discord/src/main/java/com/duuuuardo/singr/SingrLauncher.java
+++ discord/src/main/java/com/duuuuardo/singr/SingrLauncher.java
@@ -1,9 +1,40 @@
 package com.duuuuardo.singr;
 
+import com.duuuuardo.singr.utils.ConfigUtils;
+import com.duuuuardo.singr.utils.config.DiscordConfig;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 public class SingrLauncher {
   public static void main(String[] args) {
-    Singr singr = new Singr();
+    var configurationFile = new File("./discord.yaml");
 
+    if (!configurationFile.exists()) {
+      System.out.println("Welcome to Singr Launcher!");
+      System.out.println("");
+      System.out.println("Some configuration files were created in the executable folder!");
+      System.out.println("");
+      System.out.println("Fill all files with the needed configuration for the bot.");
+      System.out.println("After completing theese steps you can run the jar again!");
+
+      copyFileFromJar("/discord.yaml", "./discord.yaml");
+      System.exit(1);
+      return;
+    }
+
+    DiscordConfig config = ConfigUtils.readConfigurationFromFile(configurationFile);
+
+    Singr singr = new Singr(config);
     singr.start();
+  }
+
+  private static void copyFileFromJar(String input, String output) {
+    var inputStream = SingrLauncher.class.getResourceAsStream(input);
+    try (FileOutputStream file = new FileOutputStream(output)) {
+      file.write(inputStream.readAllBytes());
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
   }
 }
