577b6318f2c4a558dbc087303b972da14ddcb080
Duuuuardo <oeduardoef@gmail.com>
2023-07-19 03:57:23 -0300

    adding runnable jar support

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,12 @@
+pluginManagement {
+  repositories {
+    maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+    gradlePluginPortal()
+  }
+}
+
 plugins { id("org.gradle.toolchains.foojay-resolver-convention") version "0.4.0" }
 
-rootProject.name = "Singr"
+rootProject.name = "singr-parent"
 
 include(":discord")
